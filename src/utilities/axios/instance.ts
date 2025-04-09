539ae9213f9c47fcbcfdbaee4054d9487051312c
Anurudhan <anurudhanvaliyakath@gmail.com>
2025-04-09 10:58:59 +0530

    deployment issue resolved

--- src/utilities/axios/instance.ts
+++ src/utilities/axios/instance.ts
@@ -1,11 +1,7 @@
 import axios from 'axios'
 import { env } from '../../common/env';
 
-const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-
-export const BASE_URL = isDevelopment 
-  ? String(env.API_GATEWAY_URL)
-  : ""
+export const BASE_URL = String(env.API_GATEWAY_URL);
 
   console.log(BASE_URL,"hee");
   
